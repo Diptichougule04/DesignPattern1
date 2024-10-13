f9a256d808d701514d8e72cadf6e77c63e7bea13
Diptichougule04 <144772981+Diptichougule04@users.noreply.github.com>
2024-10-13 13:04:23 +0530

    Add project files.

---
+++ DesignPattern1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPattern1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Singleton s1= Singleton.GetInstance();
+            s1.getDatabaseObject().stringSet("Cricketer", "Rohit Sharma");
+
+            Singleton s2= Singleton.GetInstance();
+
+            Console.WriteLine("Hello");
+
+            
+            Console.ReadLine();
+        }
+    }
+}
---
+++ DesignPattern1/Singleton.cs
@@ -0,0 +1,42 @@
+﻿using StackExchange.Redis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace DesignPattern1
+{
+    internal class Singleton
+    {
+        //1declere private static field
+        private static Singleton instance;
+
+        private static ConnectionMultiplexer conn;
+        private static 
+
+        //2 private constructor
+        private Singleton()
+        {
+        conn=
+        }
+
+        //3getInstance method which will be accessed from outside
+        //will return instance always when it called
+        //if 1st time call create instance
+        //else return always  created
+
+        public static Singleton GetInstance()
+        { 
+            if (instance == null)
+            {
+                instance = new Singleton();
+            }
+            return instance;
+        }
+        //4any business logic(optional)
+        public IDatabase
+
+    }
+}
